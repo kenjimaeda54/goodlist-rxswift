0c3614274aa811b7dd9dcbd68f7511dd8687c6dc
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-10-21 08:13:40 -0300

    Initial Commit

---
+++ GoodList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodList
+//
+//  Created by kenjimaeda on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
